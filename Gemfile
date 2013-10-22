8593dfb125c5aadf13de53610c23953360b234dc
Spencer Rogers <spencer1248@gmail.com>
2013-10-22 12:07:03 -0400

    Create Gemfile tests

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'hashie'
+gem 'sinatra', '1.4.4'
+gem 'octokit', '~> 2.0'
+
+gem 'pry', group: :development
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/bundler_spec.rb
@@ -0,0 +1,67 @@
+describe "Bundler" do
+  describe "Gemfile" do
+    before :all do
+      @gemfile_text = File.read('Gemfile')
+    end
+
+    # http://bundler.io/v1.3/gemfile.html
+    it "should specify rubygems as a source using the SSL protocol" do
+      (@gemfile_text =~ /source .https:\/\/rubygems.org./).should_not == nil
+    end
+
+    # http://bundler.io/v1.3/gemfile.html
+    it "should list the hashie gem without specifying a version" do
+      (@gemfile_text =~ /gem .hashie./).should_not == nil
+    end
+
+    # http://bundler.io/v1.3/gemfile.html
+    it "should list the sinatra gem with the specific version 1.4.4" do
+      (@gemfile_text =~ /gem .sinatra., .v?1\.4\.4./).should_not == nil
+    end
+
+    # http://robots.thoughtbot.com/post/2508037841/rubys-pessimistic-operator
+    it "list octokit specifying the version 2.0 with a twiddle-wakka" do
+      (@gemfile_text =~ /gem .octokit.*.~> 2\.0./).should_not == nil
+    end
+
+    describe "groups" do
+      before :each do
+        `bundle --without nothing`
+      end
+
+      # http://bundler.io/v1.3/groups.html
+      # Use the updated Hash syntax { foo: bar }
+      it "should contain the pry gem in the development group using a hash argument to the gem method" do
+        (@gemfile_text =~ /gem .pry., group: :development'?/).should_not == nil
+
+        bundle_output = `bundle`
+        (bundle_output =~ /pry/).should_not == nil
+
+        bundle_output = `bundle --without development`
+        (bundle_output =~ /pry/).should == nil
+      end
+
+      # http://bundler.io/v1.3/groups.html
+      # Use the updated Hash syntax { foo: bar }
+      it "should contain the rspec gem in the test group using block syntax" do
+        (@gemfile_text =~ /group .?test.* do/).should_not == nil
+
+        bundle_output = `bundle`
+        (bundle_output =~ /rspec/).should_not == nil
+
+        bundle_output = `bundle --without test`
+        (bundle_output =~ /rspec/).should == nil
+      end
+    end
+  end
+
+  # manually run bundle install to get this passing
+  # (or bundle for short)
+  describe "bundle install" do
+    describe "Gemfile.lock" do
+      it "should exist after running `bundle install`" do
+        File.exists?('Gemfile.lock').should == true
+      end
+    end
+  end
+end
